f04be2ed6803be37a653c1b6b531a4cde41099f8
zeinrz <dino.zhanjoechristian@gmail.com>
2026-01-02 21:57:22 +0800

    Add mapFunction for handling map-popup display
    Added mapFunction to handle map-popup display logic.

--- path/donation.js
+++ path/donation.js
@@ -12,4 +12,19 @@ function onlineDonation(toggle){
         });
     }
 }
+
 console.log("hello world");
+function mapFunction(toggle){
+    console.log('toggle');
+    const poppup = document.querySelectorAll('[id=map-popup]');
+    if(toggle == 1){
+        poppup.forEach(function(element){
+            console.log('toggle');
+            element.style.display = '';
+        });
+    }else{
+        poppup.forEach(function(element){
+            element.style.display = 'none';
+        });
+    }
+}
